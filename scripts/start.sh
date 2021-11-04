02de3768524b0ffc3a7b3c02bdb58316e137b9ba
Louis-Eric Tremblay <louis-eric.tremblay@ssc-spc.gc.ca>
2021-11-04 13:54:23 +0000

    Updated start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,9 @@
 ./wait
 echo "Starting application in ${NODE_ENV} environment"
 
-echo "Starting mongodb service"
-service mongodb start
-echo "Started mongodb service"
+#echo "Starting mongodb service"
+#service mongodb start
+#echo "Started mongodb service"
 
 if [ "$WAIT_FOR_MONGO" == "true" ]
 then
@@ -17,9 +17,9 @@ echo "Mongo DB started, starting application"
 
 if [ "$POPULATE_DB"  == "true" ]
 then
-    echo "Populating Mongo Database"
-    ./mongotools/bin/mongorestore --uri=${DB_URI} dump/
-    echo "database populated"
+ #   echo "Populating Mongo Database"
+ #   ./mongotools/bin/mongorestore --uri=${DB_URI} dump/
+ #   echo "database populated"
 else
     echo "Populating false"
 fi
