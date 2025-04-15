77346a3154b0a1fec2a56536ffa364f7c4a01544
Shawn Thompson <plansmash@me.com>
2025-04-15 13:33:06 -0400

    remove mongodb from docker, it's not used in production
    update trigger for this pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 echo "Starting application in ${NODE_ENV} environment"
 
-echo "Starting mongodb service"
-service mongodb start
-echo "Started mongodb service"
-
-if [ "$WAIT_FOR_MONGO" == "true" ]
-then
-    echo "Waiting for MongoDB to start"
-    ./wait 
-else
-    echo "Waiting false"
-fi 
-echo "Mongo DB started, starting application"
-
-#if [ "$POPULATE_DB"  == "true" ]
-#then
- #   echo "Populating Mongo Database"
- #   ./mongotools/bin/mongorestore --uri=${DB_URI} dump/
- #   echo "database populated"
-#else
-#    echo "Populating false"
-#fi
-
 echo "Starting sshd service"
 /usr/sbin/sshd
 echo "Started sshd service"
