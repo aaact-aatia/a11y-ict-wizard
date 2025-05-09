246b43e0c2ca57748caa35f3c7ea3b8ba1708aa3
Shawn Thompson <plansmash@me.com>
2025-05-09 15:26:42 -0400

    remove sass from project

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const sassMiddleware = require("node-sass-middleware");
 const mongoose = require("mongoose");
 const auth = require("http-auth");
 
@@ -31,14 +30,6 @@ app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
-app.use(
-	sassMiddleware({
-		src: path.join(__dirname, "public"),
-		dest: path.join(__dirname, "public"),
-		indentedSyntax: false, // true = .sass and false = .scss
-		sourceMap: true,
-	})
-);
 app.use(express.static(path.join(__dirname, "public")));
 
 // Simple authorization for edit routes
